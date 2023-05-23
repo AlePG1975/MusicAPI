0175b12758bc3de6f0b3a7facda5aecb580c35c6
AlePG1975 <134084912+AlePG1975@users.noreply.github.com>
2023-05-23 10:32:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def index():
 
 @app.get("/MusicAPI/{num}")
 def musicapi(num):
+    music={
+        "1":"Dealer",
+        "2":"Say Yes To Have",
+        "3":"Radio",
+        "4":"Art Deco",
+        
+    }
+    return music{num}
     return num
 
 @app.get("/Conversor_CaF/{C}")
@@ -18,3 +26,21 @@ def conversorCaF(C):
           return f"La temperatura es de:{TF}grados Fahrenheit"
     exept:
             return "Entrada invalida"
+@app.get("/RevisarEdad/{E1}/{E2}")
+def revisar_edades(E1,E2):
+    E1=int(E1)
+    E2=int(E2)
+    if E1>E2+30:
+        return "Podrias ser su Abuelo"
+    elif E1>E2+15:
+        return "Podrias ser su padre"
+    elif E1>E2:
+        return "Eres mayor"
+    if E1>E2+30:
+        return "Podria ser su Abuelo"
+    elif E1>E2+15:
+        return "Podria ser su padre"
+    elif E1>E2:
+        return "Eres mayor que tu"
+else:
+    return "Tiene la misma edad"
