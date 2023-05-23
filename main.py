25d38d36fd9c2c05df0a46b67edf85366a29a83d
AlePG1975 <134084912+AlePG1975@users.noreply.github.com>
2023-05-23 09:55:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ def index():
 @app.get("/MusicAPI/{num}")
 def musicapi(num):
     return num
+
+@app.get("/Conversor_CaF/{C}")
+def conversorCaF(C):
+   TF=C*(9/5) + 32
+return f"La temperatura es de:{TF]grados Fahrenheit"
+    
